408f271a66128e9817218c421a860f9a9318818c
Daniel Wald <daniel.markersdorf@gmail.com>
2021-10-16 10:45:52 +0000

    Translated using Weblate (Czech)
    Currently translated at 11.1% (1 of 9 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/cs/

--- Messages/cs.php
+++ Messages/cs.php
@@ -1,1 +1,10 @@
 <?php
+return [
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 10 2019
+ */
+    'repModuleAutoprovision' => '%repesent%',
+];
