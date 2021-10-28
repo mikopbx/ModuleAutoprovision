a6f2b1e5b16f1b1a207352aee478ffe7df2a81c1
Sergey Dotsenko <s.docent@gmail.com>
2021-10-28 01:18:32 +0000

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/uk/

--- Messages/uk.php
+++ Messages/uk.php
@@ -1,1 +1,18 @@
 <?php
+return [
+    'mod_Autoprovision_additional_params' => 'Додаткові параметри',
+    'mod_Autoprovision_mac_white' => 'Білий список MAC адрес телефонів',
+    'mod_Autoprovision_mac_black' => 'Блакитний список MAC адрес телефонів',
+    'mod_Autoprovision_pbx_host' => 'Адреса сервера для реєстрації телефонів',
+    'mod_Autoprovision_Extension' => 'Внутрішній номер шаблон',
+    'SubHeaderModuleAutoprovision' => 'Допомога в налаштуванні SIP телефонів',
+    'BreadcrumbModuleAutoprovision' => 'Модуль автоматичного налаштування телефонів',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 10 2019
+ */
+    'repModuleAutoprovision' => 'Модуль - %repesent%',
+    'mo_ModuleAutoprovision' => 'Модуль автоматичного налаштування телефонів',
+];
