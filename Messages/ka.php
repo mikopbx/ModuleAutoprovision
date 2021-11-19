811d552ce25875da791376492cc740bc5f43001a
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:26:21 +0000

    Translated using Weblate (Georgian)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/ka/

--- Messages/ka.php
+++ Messages/ka.php
@@ -1,1 +1,18 @@
 <?php
+return [
+    'mod_Autoprovision_additional_params' => 'დამატებითი პარამეტრები',
+    'mod_Autoprovision_mac_white' => 'ტელეფონის MAC მისამართების თეთრი სია',
+    'mod_Autoprovision_mac_black' => 'ტელეფონების MAC მისამართების შავი სია',
+    'mod_Autoprovision_pbx_host' => 'სერვერის მისამართი ტელეფონის რეგისტრაციისთვის',
+    'mod_Autoprovision_Extension' => 'გაფართოების ნომრის შაბლონი',
+    'SubHeaderModuleAutoprovision' => 'დახმარება SIP ტელეფონების დაყენებაში',
+    'BreadcrumbModuleAutoprovision' => 'ტელეფონის ავტომატური დაყენების მოდული',
+    'mo_ModuleAutoprovision' => 'ტელეფონის ავტომატური დაყენების მოდული',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 10 2019
+ */
+    'repModuleAutoprovision' => 'მოდული -% რეპესენტი%',
+];
