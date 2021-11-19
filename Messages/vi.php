1e3e20e7728e6cebd9e0869d96821167c68f76c0
Nikolay Beketov <nbek@miko.ru>
2021-11-19 11:21:19 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/vi/

--- Messages/vi.php
+++ Messages/vi.php
@@ -1,1 +1,18 @@
 <?php
+return [
+    'mod_Autoprovision_Extension' => 'Mẫu số máy lẻ',
+    'SubHeaderModuleAutoprovision' => 'Trợ giúp trong việc thiết lập điện thoại SIP',
+    'BreadcrumbModuleAutoprovision' => 'Mô-đun cài đặt điện thoại tự động',
+    'mo_ModuleAutoprovision' => 'Mô-đun cài đặt điện thoại tự động',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 10 2019
+ */
+    'repModuleAutoprovision' => 'Mô-đun -% repesent%',
+    'mod_Autoprovision_additional_params' => 'Tùy chọn bổ sung',
+    'mod_Autoprovision_mac_white' => 'Danh sách cho phép địa chỉ MAC của điện thoại',
+    'mod_Autoprovision_mac_black' => 'Danh sách đen địa chỉ MAC của điện thoại',
+    'mod_Autoprovision_pbx_host' => 'Địa chỉ máy chủ để đăng ký điện thoại',
+];
