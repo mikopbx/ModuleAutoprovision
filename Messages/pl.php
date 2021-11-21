8f31efd141c6b088c3657aab8f9a4586bc6cac68
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:14:29 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/pl/

--- Messages/pl.php
+++ Messages/pl.php
@@ -1,1 +1,18 @@
 <?php
+return [
+    'mod_Autoprovision_additional_params' => 'Dodatkowe opcje',
+    'mod_Autoprovision_mac_white' => 'Biała lista adresów MAC telefonu',
+    'mod_Autoprovision_mac_black' => 'Czarna lista adresów MAC telefonów',
+    'mod_Autoprovision_pbx_host' => 'Adres serwera do rejestracji telefonu',
+    'mod_Autoprovision_Extension' => 'Szablon numeru wewnętrznego',
+    'SubHeaderModuleAutoprovision' => 'Pomoc w konfiguracji telefonów SIP',
+    'BreadcrumbModuleAutoprovision' => 'Moduł automatycznej konfiguracji telefonu',
+    'mo_ModuleAutoprovision' => 'Moduł automatycznej konfiguracji telefonu',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 10 2019
+ */
+    'repModuleAutoprovision' => 'Moduł -% reprezentatywnych%',
+];
