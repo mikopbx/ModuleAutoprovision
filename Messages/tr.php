f888ad670191a549b1290623828b74f1b6a6385b
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:25:46 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/tr/

--- Messages/tr.php
+++ Messages/tr.php
@@ -1,1 +1,18 @@
 <?php
+return [
+    'mod_Autoprovision_additional_params' => 'Ek seçenekler',
+    'mod_Autoprovision_mac_white' => 'Telefon MAC adresi beyaz listesi',
+    'mod_Autoprovision_mac_black' => 'Telefonların MAC adreslerinin kara listesi',
+    'mod_Autoprovision_pbx_host' => 'Telefon kaydı için sunucu adresi',
+    'mod_Autoprovision_Extension' => 'Dahili numara şablonu',
+    'SubHeaderModuleAutoprovision' => 'SIP telefonlarının kurulumunda yardım',
+    'BreadcrumbModuleAutoprovision' => 'Otomatik telefon kurulum modülü',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 10 2019
+ */
+    'repModuleAutoprovision' => 'Modül -% tekrarlama%',
+    'mo_ModuleAutoprovision' => 'Otomatik telefon kurulum modülü',
+];
