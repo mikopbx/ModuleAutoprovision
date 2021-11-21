2b241dad0b3b517798b6e5663726c84c87e72e4b
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:18:17 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/es/

--- Messages/es.php
+++ Messages/es.php
@@ -1,1 +1,18 @@
 <?php
+return [
+    'mo_ModuleAutoprovision' => 'Módulo de configuración automática de teléfono',
+    'mod_Autoprovision_additional_params' => 'Opciones adicionales',
+    'mod_Autoprovision_mac_white' => 'Lista blanca de direcciones MAC del teléfono',
+    'mod_Autoprovision_mac_black' => 'Lista negra de direcciones MAC de teléfonos',
+    'mod_Autoprovision_pbx_host' => 'Dirección del servidor para el registro telefónico',
+    'mod_Autoprovision_Extension' => 'Plantilla de número de extensión',
+    'SubHeaderModuleAutoprovision' => 'Ayuda para configurar teléfonos SIP',
+    'BreadcrumbModuleAutoprovision' => 'Módulo de configuración automática de teléfono',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 10 2019
+ */
+    'repModuleAutoprovision' => 'Módulo -% repesent%',
+];
