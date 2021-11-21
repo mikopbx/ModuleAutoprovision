79e72d52305555aa26573ba5a4ce5a35866c4758
Nikolay Beketov <nbek@miko.ru>
2021-11-21 15:35:15 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/cs/

--- Messages/cs.php
+++ Messages/cs.php
@@ -7,4 +7,12 @@ return [
  * Written by Alexey Portnov, 10 2019
  */
     'repModuleAutoprovision' => '%repesent%',
+    'mod_Autoprovision_mac_white' => 'Seznam povolených MAC adres telefonu',
+    'mod_Autoprovision_mac_black' => 'Černá listina MAC adres telefonů',
+    'mod_Autoprovision_pbx_host' => 'Adresa serveru pro telefonickou registraci',
+    'mod_Autoprovision_Extension' => 'Šablona čísla přípony',
+    'mo_ModuleAutoprovision' => 'Modul automatického nastavení telefonu',
+    'SubHeaderModuleAutoprovision' => 'Pomoc s nastavením SIP telefonů',
+    'BreadcrumbModuleAutoprovision' => 'Modul automatického nastavení telefonu',
+    'mod_Autoprovision_additional_params' => 'Další možnosti',
 ];
