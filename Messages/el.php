e67a46dedc55854adba6f341950b21b2b312469d
Voutsas Theocharis <voutsasth@yahoo.gr>
2022-06-16 18:06:40 +0000

    Translated using Weblate (Greek)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/el/

--- Messages/el.php
+++ Messages/el.php
@@ -1,1 +1,18 @@
 <?php
+return [
+    'mod_Autoprovision_additional_params' => 'Επιπλέον επιλογές',
+    'mod_Autoprovision_mac_white' => 'Λευκή λίστα διευθύνσεων MAC τηλεφώνου',
+    'mod_Autoprovision_mac_black' => 'Μαύρη λίστα MAC τηλεφώνου',
+    'mod_Autoprovision_pbx_host' => 'Διεύθυνση διακομιστή για εγγραφή τηλεφώνου',
+    'mod_Autoprovision_Extension' => 'Πρότυπο επέκτασης',
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 10 2019
+ */
+    'repModuleAutoprovision' => 'Ενότητα Autoprovision',
+    'SubHeaderModuleAutoprovision' => 'Βοήθεια στη ρύθμιση τηλεφώνων SIP',
+    'BreadcrumbModuleAutoprovision' => 'Μονάδα αυτόματης διαμόρφωσης τηλεφώνου',
+    'mo_ModuleAutoprovision' => 'Μονάδα αυτόματης διαμόρφωσης τηλεφώνου',
+];
