db263a3c4eeb35aca8b8ccb952ec10c8b017f4e3
nb <nbek@miko.ru>
2023-05-16 14:54:36 +0700

    Translate comments into English rewrite phpdoc blocks #522

--- Lib/AutoprovisionConf.php
+++ Lib/AutoprovisionConf.php
@@ -39,7 +39,7 @@ class AutoprovisionConf extends ConfigClass
      *
      * @param array $request
      *
-     * @return PBXApiResult
+     * @return PBXApiResult An object containing the result of the API call.
      */
     public function moduleRestAPICallback(array $request): PBXApiResult
     {
