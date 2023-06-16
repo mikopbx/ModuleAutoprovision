8460646cc433b2e2ebd2b872d3ffb3fbf12c7eb0
nb <nbek@miko.ru>
2023-06-16 11:43:28 +0700

    #511 Fix module translation after installation

--- Setup/PbxExtensionSetup.php
+++ Setup/PbxExtensionSetup.php
@@ -1,4 +1,22 @@
 <?php
+/*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /**
  * Copyright © MIKO LLC - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
@@ -15,7 +33,6 @@ use MikoPBX\Common\Models\PbxSettings;
 use Modules\ModuleAutoprovision\Models\ModuleAutoprovision;
 use MikoPBX\Modules\Setup\PbxExtensionSetupBase;
 use Throwable;
-
 
 class PbxExtensionSetup extends PbxExtensionSetupBase
 {
