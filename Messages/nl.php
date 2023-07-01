7bf858ddbf7d187f0ffe9b379c4a58aadb8a0524
Jochem Pluim <jochem@cafedoranjeboom.nl>
2023-07-01 23:16:23 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/nl/

--- Messages/nl.php
+++ Messages/nl.php
@@ -1,1 +1,41 @@
 <?php
+return [
+    'mod_Autoprovision_header' => 'Als de module is ingeschakeld, wordt het SIP-account "<b>apv-miko-pbx</b>" beschikbaar op de PBX.
+<br>Als u uw telefoon automatisch wilt configureren, moet u deze terugzetten naar de fabrieksinstellingen.
+<br>Als de telefoon voor de eerste keer verbinding maakt met de PBX, wordt deze geregistreerd bij de "<b>apv-miko-pbx</b>"-account.
+<br>Om de telefoon te configureren, moet u vanaf de telefoon "<b>%extension%</b>" bellen, waarbij XXX het interne nummer op de PBX is.
+<br><br>
+Automatische configuratie is alleen mogelijk voor toestellen in hetzelfde lokale netwerk, voor telefoons van <b>Yealink, Snom, Fanvil</b>.',
+    'mod_Autoprovision_additional_params' => 'Overige instellingen',
+    'mod_Autoprovision_mac_white' => 'MAC-whitelist',
+    'mod_Autoprovision_mac_black' => 'MAC-blacklist',
+    'mod_Autoprovision_pbx_host' => 'PBX DNS naam',
+    'mod_Autoprovision_Extension' => 'Instelpatroon',
+    'SubHeaderModuleAutoprovision' => 'Bulk IP-telefoon installatie',
+    'BreadcrumbModuleAutoprovision' => 'Autoprovision module',
+    'mo_ModuleAutoprovision' => 'Autoprovision module',
+    /*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+    /**
+ * Copyright © MIKO LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alexey Portnov, 10 2019
+ */
+    'repModuleAutoprovision' => 'Module - %repesent%',
+];
