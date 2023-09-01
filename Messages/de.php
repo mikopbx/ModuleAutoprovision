887917fb22c3300ef3c84cfae827a9702240b516
Anonymous <noreply@weblate.org>
2023-09-01 11:55:13 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/de/

--- Messages/de.php
+++ Messages/de.php
@@ -15,4 +15,10 @@ return [
  * Written by Alexey Portnov, 10 2019
  */
     'repModuleAutoprovision' => 'Modul -%repesent%',
+    'mod_Autoprovision_header' => 'Wenn das Modul aktiviert ist, wird das SIP-Konto „<b>apv-miko-pbx</b>“ auf der TK-Anlage verfügbar.
+<br>Um Ihr Telefon automatisch zu konfigurieren, müssen Sie es auf die Werkseinstellungen zurücksetzen.
+<br>Wenn das Telefon zum ersten Mal eine Verbindung zur Telefonanlage herstellt, wird es im Konto „<b>apv-miko-pbx</b>“ registriert.
+<br>Um das Telefon zu konfigurieren, müssen Sie von dort aus „<b>%extension%</b>“ anrufen, wobei XXX die interne Nummer der Telefonanlage ist.
+<br><br>
+Die automatische Konfiguration ist nur für das lokale Netzwerk des Unternehmens und für Telefone <b>Yealink, Snom, Fanvil</b> möglich.',
 ];
