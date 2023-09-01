288f07569ff9a7bec27c212cee40d306c8949089
Anonymous <noreply@weblate.org>
2023-09-01 11:55:12 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/vi/

--- Messages/vi.php
+++ Messages/vi.php
@@ -15,4 +15,10 @@ return [
     'mod_Autoprovision_mac_white' => 'Danh sách cho phép địa chỉ MAC của điện thoại',
     'mod_Autoprovision_mac_black' => 'Danh sách đen địa chỉ MAC của điện thoại',
     'mod_Autoprovision_pbx_host' => 'Địa chỉ máy chủ để đăng ký điện thoại',
+    'mod_Autoprovision_header' => 'Nếu mô-đun được bật, tài khoản SIP "<b>apv-miko-pbx</b>" sẽ khả dụng trên PBX.
+<br>Để tự động định cấu hình điện thoại của bạn, bạn cần đặt lại điện thoại về cài đặt gốc.
+<br>Nếu điện thoại kết nối với PBX lần đầu tiên, nó sẽ được đăng ký vào tài khoản "<b>apv-miko-pbx</b>".
+<br>Để định cấu hình điện thoại, bạn cần gọi "<b>%extension%</b>" từ điện thoại đó, trong đó XXX là số nội bộ trên PBX.
+<br><br>
+Tự động cấu hình chỉ có thể thực hiện được đối với mạng cục bộ của doanh nghiệp, dành cho điện thoại <b>Yealink, Snom, Fanvil</b>.',
 ];
