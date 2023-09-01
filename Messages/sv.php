b4c1fe15a980c57f5a187a1b1ed7c27e52bcf231
Anonymous <noreply@weblate.org>
2023-09-01 11:55:13 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/sv/

--- Messages/sv.php
+++ Messages/sv.php
@@ -15,4 +15,10 @@ return [
     'mod_Autoprovision_mac_black' => 'Svartlista över MAC-adresser för telefoner',
     'SubHeaderModuleAutoprovision' => 'Hjälp med att ställa in SIP-telefoner',
     'BreadcrumbModuleAutoprovision' => 'Automatisk telefoninställningsmodul',
+    'mod_Autoprovision_header' => 'Om modulen är aktiverad blir SIP-kontot "<b>apv-miko-pbx</b>" tillgängligt på telefonväxeln.
+<br>För att konfigurera din telefon automatiskt måste du återställa den till fabriksinställningarna.
+<br>Om telefonen ansluter till telefonväxeln för första gången kommer den att registreras på "<b>apv-miko-pbx</b>"-kontot.
+<br>För att konfigurera telefonen måste du ringa "<b>%extension%</b>" från den, där XXX är det interna numret på telefonväxeln.
+<br><br>
+Autokonfiguration är endast möjlig för företagets lokala nätverk, för telefoner <b>Yealink, Snom, Fanvil</b>.',
 ];
