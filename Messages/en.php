58b8b6d15dfdb97c7ce922d87b92f15851483ecc
Anonymous <noreply@weblate.org>
2023-09-01 11:55:12 +0000

    Translated using Weblate (English)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/en/

--- Messages/en.php
+++ Messages/en.php
@@ -1,19 +1,24 @@
 <?php
-/**
+return [
+    /**
  * Copyright © MIKO LLC - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Alexey Portnov, 10 2019
  */
-
-return [
-    'repModuleAutoprovision'              => 'Autoprovision module - %repesent%',
-    'mo_ModuleAutoprovision'              => 'The autoprovision module',
-    'BreadcrumbModuleAutoprovision'       => 'The autoprovision module',
-    'SubHeaderModuleAutoprovision'        => 'Bulk ip-phones setup',
-    'mod_Autoprovision_Extension'         => 'Provision pattern command',
-    'mod_Autoprovision_pbx_host'          => 'The PBX DNS name',
-    'mod_Autoprovision_mac_black'         => 'Black MAC address list',
-    'mod_Autoprovision_mac_white'         => 'White MAC address list',
+    'repModuleAutoprovision' => 'Autoprovision module - %repesent%',
+    'mo_ModuleAutoprovision' => 'The autoprovision module',
+    'BreadcrumbModuleAutoprovision' => 'The autoprovision module',
+    'SubHeaderModuleAutoprovision' => 'Bulk ip-phones setup',
+    'mod_Autoprovision_Extension' => 'Provision pattern command',
+    'mod_Autoprovision_pbx_host' => 'The PBX DNS name',
+    'mod_Autoprovision_mac_black' => 'Black MAC address list',
+    'mod_Autoprovision_mac_white' => 'White MAC address list',
     'mod_Autoprovision_additional_params' => 'Additional settings',
+    'mod_Autoprovision_header' => 'If the module is enabled, the SIP account "<b>apv-miko-pbx</b>" becomes available on the PBX.
+<br>To automatically configure your phone, you need to reset it to factory settings.
+<br>If the phone connects to the PBX for the first time, it will be registered to the "<b>apv-miko-pbx</b>" account.
+<br>To configure the phone, you need to call "<b>%extension%</b>" from it, where XXX is the internal number on the PBX.
+<br><br>
+Autoconfiguration is possible only for the local network of the enterprise, for phones <b>Yealink, Snom, Fanvil</b>.',
 ];
