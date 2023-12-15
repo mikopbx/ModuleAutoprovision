3f326697695c7946438416c28b602877776ff92c
Anonymous <noreply@weblate.org>
2023-12-15 12:07:13 +0000

    Translated using Weblate (Czech)
    Currently translated at 96.5% (28 of 29 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/cs/

--- Messages/cs.php
+++ Messages/cs.php
@@ -21,4 +21,23 @@ return [
 <br>Pro konfiguraci telefonu z něj musíte zavolat na "<b>%extension%</b>", kde XXX je interní číslo ústředny.
 <br><br>
 Automatická konfigurace je možná pouze pro lokální síť podniku, pro telefony <b>Yealink, Snom, Fanvil</b>.',
+    'mod_Autoprovision_templates_users_header' => 'Při popisu MAC adresy je povoleno používat symbol <b>%</b> – což znamená „jakákoli sada znaků“ <br>
+Šablona <b>805e0c67 %</b> bude odpovídat <b>805e0c670001</b> a <b>805e0c670002</b>',
+    'mod_Autoprovision_phone_settings_title' => 'Nastavení telefonu',
+    'mod_Autoprovision_phone_templates' => 'Šablony nastavení',
+    'mod_Autoprovision_general_settings' => 'Nastavení URI',
+    'mod_Autoprovision_pnp' => 'Nastavení PnP',
+    'mod_Autoprovision_addNew' => 'Přidat',
+    'mod_Autoprovision_templates_uri_uri' => 'URI',
+    'mod_Autoprovision_templates_uri_template' => 'Vzorek',
+    'mod_Autoprovision_phone_settings_user' => 'Zaměstnanec',
+    'mod_Autoprovision_phone_settings_mac' => 'MAC adresa',
+    'mod_Autoprovision_template_name' => 'název',
+    'mod_Autoprovision_search_tags' => 'Vyhledávání...',
+    'mod_Autoprovision_edit_template' => 'Úprava šablony',
+    'mod_Autoprovision_end_edit_template' => 'Dokončete úpravy',
+    'mod_Autoprovision_other_pbx' => 'Telefonní seznam',
+    'mod_Autoprovision_other_pbx_name' => 'Název telefonní ústředny',
+    'mod_Autoprovision_other_pbx_address' => 'Síťová adresa PBX',
+    'mod_Autoprovision_templates_header' => 'Při popisu šablony můžete použít následující parametry: <b>{SIP_USER_NAME}</b> - jméno zaměstnance <b>{SIP_NUM}</b> - interní číslo (login) <b>{SIP_PASS}</b> - heslo',
 ];
