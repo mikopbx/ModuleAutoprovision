95af43cc01f8c1ca90e4fd3d0df38d8b494f6b85
Anonymous <noreply@weblate.org>
2023-12-15 12:07:33 +0000

    Translated using Weblate (Georgian)
    Currently translated at 96.5% (28 of 29 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/ka/

--- Messages/ka.php
+++ Messages/ka.php
@@ -21,4 +21,23 @@ return [
 <br>ტელეფონის კონფიგურაციისთვის, თქვენ უნდა დარეკოთ მისგან "<b>%extension%</b>", სადაც XXX არის PBX-ის შიდა ნომერი.
 <br><br>
 ავტოკონფიგურაცია შესაძლებელია მხოლოდ საწარმოს ლოკალური ქსელისთვის, ტელეფონებისთვის <b>Yealink, Snom, Fanvil</b>.',
+    'mod_Autoprovision_phone_settings_title' => 'ტელეფონის პარამეტრები',
+    'mod_Autoprovision_phone_templates' => 'პარამეტრების შაბლონები',
+    'mod_Autoprovision_general_settings' => 'URI პარამეტრები',
+    'mod_Autoprovision_pnp' => 'PnP პარამეტრები',
+    'mod_Autoprovision_addNew' => 'დამატება',
+    'mod_Autoprovision_templates_uri_uri' => 'URI',
+    'mod_Autoprovision_templates_uri_template' => 'ნიმუში',
+    'mod_Autoprovision_templates_users_header' => 'MAC მისამართის აღწერისას ნებადართულია სიმბოლო <b>%</b> - რაც ნიშნავს "ნებისმიერი სიმბოლოების კომპლექტს" <br>
+შაბლონი <b>805e0c67%</b> დაემთხვევა <b>805e0c670001</b> და <b>805e0c670002</b>',
+    'mod_Autoprovision_phone_settings_user' => 'თანამშრომელი',
+    'mod_Autoprovision_phone_settings_mac' => 'Mac მისამართი',
+    'mod_Autoprovision_template_name' => 'სახელი',
+    'mod_Autoprovision_search_tags' => 'ძიება...',
+    'mod_Autoprovision_edit_template' => 'შაბლონის რედაქტირება',
+    'mod_Autoprovision_end_edit_template' => 'დაასრულეთ რედაქტირება',
+    'mod_Autoprovision_other_pbx' => 'Სატელეფონო წიგნი',
+    'mod_Autoprovision_other_pbx_name' => 'სატელეფონო სადგურის დასახელება',
+    'mod_Autoprovision_other_pbx_address' => 'PBX ქსელის მისამართი',
+    'mod_Autoprovision_templates_header' => 'შაბლონის აღწერისას შეგიძლიათ გამოიყენოთ შემდეგი პარამეტრები: <b>{SIP_USER_NAME}</b> - თანამშრომლის სახელი <b>{SIP_NUM}</b> - შიდა ნომერი (შესვლა) <b>{SIP_PASS}</b> - პაროლი',
 ];
