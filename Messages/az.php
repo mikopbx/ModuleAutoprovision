45f9b4e24b6579fd7b851ef37b5a8817065dc380
Anonymous <noreply@weblate.org>
2023-12-15 12:07:36 +0000

    Translated using Weblate (Azerbaijani)
    Currently translated at 96.5% (28 of 29 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/az/

--- Messages/az.php
+++ Messages/az.php
@@ -38,4 +38,23 @@ return [
 <br>Telefonu konfiqurasiya etmək üçün siz ondan "<b>%extension%</b>" zəng etməlisiniz, burada XXX PBX-də daxili nömrədir.
 <br><br>
 Avtokonfiqurasiya yalnız müəssisənin yerli şəbəkəsi, <b>Yealink, Snom, Fanvil</b> telefonları üçün mümkündür.',
+    'mod_Autoprovision_phone_settings_title' => 'Telefon parametrləri',
+    'mod_Autoprovision_phone_templates' => 'Parametrlər şablonları',
+    'mod_Autoprovision_general_settings' => 'URI Parametrləri',
+    'mod_Autoprovision_pnp' => 'PnP Parametrləri',
+    'mod_Autoprovision_addNew' => 'əlavə et',
+    'mod_Autoprovision_templates_uri_uri' => 'URI',
+    'mod_Autoprovision_templates_uri_template' => 'Nümunə',
+    'mod_Autoprovision_phone_settings_user' => 'işçi',
+    'mod_Autoprovision_phone_settings_mac' => 'MAC Ünvanı',
+    'mod_Autoprovision_template_name' => 'ad',
+    'mod_Autoprovision_search_tags' => 'Axtar...',
+    'mod_Autoprovision_edit_template' => 'Şablonu redaktə etmək',
+    'mod_Autoprovision_end_edit_template' => 'Redaktəni tamamlayın',
+    'mod_Autoprovision_other_pbx' => 'Telefon kitabçası',
+    'mod_Autoprovision_other_pbx_name' => 'Telefon stansiyasının adı',
+    'mod_Autoprovision_other_pbx_address' => 'PBX şəbəkə ünvanı',
+    'mod_Autoprovision_templates_header' => 'Şablonu təsvir edərkən aşağıdakı parametrlərdən istifadə edə bilərsiniz: <b>{SIP_USER_NAME}</b> - işçi adı <b>{SIP_NUM}</b> - daxili nömrə (giriş) <b>{SIP_PASS}</b> - parol',
+    'mod_Autoprovision_templates_users_header' => 'MAC ünvanını təsvir edərkən <b>%</b> simvolundan istifadə etməyə icazə verilir - "hər hansı bir simvol dəsti" mənasını verir <br>
+<b>805e0c67%</b> şablonu <b>805e0c670001</b> və <b>805e0c670002</b> ilə uyğunlaşacaq',
 ];
