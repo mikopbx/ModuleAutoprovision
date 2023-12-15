99e5875712582576e7f9590f8518fc4f1c6d52dd
Anonymous <noreply@weblate.org>
2023-12-15 12:07:04 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 96.5% (28 of 29 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/vi/

--- Messages/vi.php
+++ Messages/vi.php
@@ -21,4 +21,23 @@ return [
 <br>Để định cấu hình điện thoại, bạn cần gọi "<b>%extension%</b>" từ điện thoại đó, trong đó XXX là số nội bộ trên PBX.
 <br><br>
 Tự động cấu hình chỉ có thể thực hiện được đối với mạng cục bộ của doanh nghiệp, dành cho điện thoại <b>Yealink, Snom, Fanvil</b>.',
+    'mod_Autoprovision_phone_settings_title' => 'Cài đặt điện thoại',
+    'mod_Autoprovision_phone_templates' => 'Mẫu cài đặt',
+    'mod_Autoprovision_general_settings' => 'Cài đặt URI',
+    'mod_Autoprovision_pnp' => 'Cài đặt PnP',
+    'mod_Autoprovision_addNew' => 'Thêm vào',
+    'mod_Autoprovision_templates_uri_uri' => 'URI',
+    'mod_Autoprovision_templates_uri_template' => 'Vật mẫu',
+    'mod_Autoprovision_phone_settings_user' => 'Người lao động',
+    'mod_Autoprovision_phone_settings_mac' => 'Địa chỉ MAC',
+    'mod_Autoprovision_template_name' => 'Tên',
+    'mod_Autoprovision_search_tags' => 'Tìm kiếm...',
+    'mod_Autoprovision_edit_template' => 'Chỉnh sửa mẫu',
+    'mod_Autoprovision_end_edit_template' => 'Hoàn tất chỉnh sửa',
+    'mod_Autoprovision_other_pbx' => 'Danh bạ điện thoại',
+    'mod_Autoprovision_other_pbx_name' => 'Tên tổng đài điện thoại',
+    'mod_Autoprovision_other_pbx_address' => 'Địa chỉ mạng PBX',
+    'mod_Autoprovision_templates_header' => 'Khi mô tả mẫu, bạn có thể sử dụng các tham số sau: <b>{SIP_USER_NAME</b> - tên nhân viên <b>{SIP_NUM</b> - số nội bộ (đăng nhập) <b>{SIP_PASS</b> - mật khẩu',
+    'mod_Autoprovision_templates_users_header' => 'Khi mô tả địa chỉ MAC, được phép sử dụng ký hiệu <b>%</b> - nghĩa là “bất kỳ bộ ký tự nào” <br>
+Mẫu <b>805e0c67%</b> sẽ khớp với <b>805e0c670001</b> và <b>805e0c670002</b>',
 ];
