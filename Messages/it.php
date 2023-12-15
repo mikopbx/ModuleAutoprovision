e82844de6317a850f28ebe9d83de64fd23715d82
Anonymous <noreply@weblate.org>
2023-12-15 12:07:22 +0000

    Translated using Weblate (Italian)
    Currently translated at 96.5% (28 of 29 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/it/

--- Messages/it.php
+++ Messages/it.php
@@ -21,4 +21,23 @@ return [
 <br>Per configurare il telefono, è necessario chiamare da esso "<b>%interno%</b>", dove XXX è il numero interno del PBX.
 <br><br>
 La configurazione automatica è possibile solo per la rete locale dell\'azienda, per i telefoni <b>Yealink, Snom, Fanvil</b>.',
+    'mod_Autoprovision_templates_uri_uri' => 'URI',
+    'mod_Autoprovision_search_tags' => 'Ricerca...',
+    'mod_Autoprovision_templates_uri_template' => 'Campione',
+    'mod_Autoprovision_phone_settings_user' => 'Dipendente',
+    'mod_Autoprovision_phone_settings_mac' => 'Indirizzo MAC',
+    'mod_Autoprovision_template_name' => 'Nome',
+    'mod_Autoprovision_edit_template' => 'Modifica di un modello',
+    'mod_Autoprovision_end_edit_template' => 'Termina la modifica',
+    'mod_Autoprovision_other_pbx' => 'Rubrica telefonica',
+    'mod_Autoprovision_other_pbx_name' => 'Nome della centrale telefonica',
+    'mod_Autoprovision_other_pbx_address' => 'Indirizzo di rete PBX',
+    'mod_Autoprovision_phone_settings_title' => 'Impostazioni del telefono',
+    'mod_Autoprovision_phone_templates' => 'Modelli di impostazioni',
+    'mod_Autoprovision_general_settings' => 'Impostazioni dell\'URI',
+    'mod_Autoprovision_pnp' => 'Impostazioni PnP',
+    'mod_Autoprovision_addNew' => 'Aggiungere',
+    'mod_Autoprovision_templates_header' => 'Quando si descrive un modello, è possibile utilizzare i seguenti parametri: <b>{SIP_USER_NAME}</b> - nome del dipendente <b>{SIP_NUM}</b> - numero interno (login) <b>{SIP_PASS}</b> - password',
+    'mod_Autoprovision_templates_users_header' => 'Quando si descrive un indirizzo MAC, è consentito utilizzare il simbolo <b>%</b> - che significa "qualsiasi insieme di caratteri" <br>
+Il modello <b>805e0c67%</b> corrisponderà a <b>805e0c670001</b> e <b>805e0c670002</b>',
 ];
