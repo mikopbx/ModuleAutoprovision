cf23692c5d0f05e58ef6fa5c1456300f121de4f5
Anonymous <noreply@weblate.org>
2023-12-15 12:06:57 +0000

    Translated using Weblate (Turkish)
    Currently translated at 96.5% (28 of 29 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/tr/

--- Messages/tr.php
+++ Messages/tr.php
@@ -21,4 +21,23 @@ return [
 <br>Telefonu yapılandırmak için "<b>%extension%</b>" telefonunu aramanız gerekir; burada XXX, PBX\'teki dahili numaradır.
 <br><br>
 Otomatik yapılandırma yalnızca işletmenin yerel ağı için, <b>Yealink, Snom, Fanvil</b> telefonları için mümkündür.',
+    'mod_Autoprovision_templates_uri_template' => 'Örnek',
+    'mod_Autoprovision_phone_settings_user' => 'Çalışan',
+    'mod_Autoprovision_phone_settings_mac' => 'Mac Adresi',
+    'mod_Autoprovision_template_name' => 'İsim',
+    'mod_Autoprovision_search_tags' => 'Aramak...',
+    'mod_Autoprovision_edit_template' => 'Şablonu düzenleme',
+    'mod_Autoprovision_phone_settings_title' => 'Telefon ayarları',
+    'mod_Autoprovision_phone_templates' => 'Ayarlar şablonları',
+    'mod_Autoprovision_general_settings' => 'URI Ayarları',
+    'mod_Autoprovision_pnp' => 'PnP Ayarları',
+    'mod_Autoprovision_addNew' => 'Eklemek',
+    'mod_Autoprovision_templates_uri_uri' => 'URI',
+    'mod_Autoprovision_end_edit_template' => 'Düzenlemeyi bitir',
+    'mod_Autoprovision_other_pbx' => 'Telefon rehberi',
+    'mod_Autoprovision_other_pbx_name' => 'Telefon santralinin adı',
+    'mod_Autoprovision_other_pbx_address' => 'PBX ağ adresi',
+    'mod_Autoprovision_templates_header' => 'Bir şablonu tanımlarken aşağıdaki parametreleri kullanabilirsiniz: <b>{SIP_USER_NAME}</b> - çalışan adı <b>{SIP_NUM}</b> - dahili numara (giriş) <b>{SIP_PASS}</b> - şifre',
+    'mod_Autoprovision_templates_users_header' => 'Bir MAC adresini tanımlarken, "herhangi bir karakter kümesi" anlamına gelen <b>%</b> sembolünün kullanılmasına izin verilir <br>
+<b>805e0c67%</b> şablonu <b>805e0c670001</b> ve <b>805e0c670002</b> ile eşleşecek',
 ];
