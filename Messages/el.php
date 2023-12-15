ccbadb4e9bd250bcbaf3a51ee2e6074d8a6f95f5
Anonymous <noreply@weblate.org>
2023-12-15 12:07:27 +0000

    Translated using Weblate (Greek)
    Currently translated at 96.5% (28 of 29 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/el/

--- Messages/el.php
+++ Messages/el.php
@@ -21,4 +21,23 @@ return [
 <br>Για να διαμορφώσετε το τηλέφωνο, πρέπει να καλέσετε το "<b>%extension%</b>" από αυτό, όπου XXX είναι ο εσωτερικός αριθμός στο PBX.
 <br><br>
 Η αυτόματη διαμόρφωση είναι δυνατή μόνο για το τοπικό δίκτυο της επιχείρησης, για τηλέφωνα <b>Yealink, Snom, Fanvil</b>.',
+    'mod_Autoprovision_phone_settings_title' => 'Ρυθμίσεις τηλεφώνου',
+    'mod_Autoprovision_phone_templates' => 'Πρότυπα ρυθμίσεων',
+    'mod_Autoprovision_general_settings' => 'Ρυθμίσεις URI',
+    'mod_Autoprovision_pnp' => 'Ρυθμίσεις PnP',
+    'mod_Autoprovision_addNew' => 'Προσθήκη',
+    'mod_Autoprovision_templates_uri_uri' => 'URI',
+    'mod_Autoprovision_templates_uri_template' => 'Δείγμα',
+    'mod_Autoprovision_phone_settings_user' => 'Υπάλληλος',
+    'mod_Autoprovision_phone_settings_mac' => 'Διεύθυνση MAC',
+    'mod_Autoprovision_template_name' => 'Ονομα',
+    'mod_Autoprovision_search_tags' => 'Αναζήτηση...',
+    'mod_Autoprovision_edit_template' => 'Επεξεργασία προτύπου',
+    'mod_Autoprovision_end_edit_template' => 'Ολοκληρώστε την επεξεργασία',
+    'mod_Autoprovision_other_pbx' => 'Τηλεφωνικό κατάλογο',
+    'mod_Autoprovision_other_pbx_name' => 'Όνομα του τηλεφωνικού κέντρου',
+    'mod_Autoprovision_other_pbx_address' => 'Διεύθυνση δικτύου PBX',
+    'mod_Autoprovision_templates_header' => 'Κατά την περιγραφή ενός προτύπου, μπορείτε να χρησιμοποιήσετε τις ακόλουθες παραμέτρους: <b>{SIP_USER_NAME}</b> - όνομα υπαλλήλου <b>{SIP_NUM}</b> - εσωτερικός αριθμός (είσοδος) <b>{SIP_PASS}</b> - κωδικός πρόσβασης',
+    'mod_Autoprovision_templates_users_header' => 'Κατά την περιγραφή μιας διεύθυνσης MAC, επιτρέπεται η χρήση του συμβόλου <b>%</b> - που σημαίνει "οποιοδήποτε σύνολο χαρακτήρων" <br>
+Το πρότυπο <b>805e0c67%</b> θα ταιριάζει με <b>805e0c670001</b> και <b>805e0c670002</b>',
 ];
