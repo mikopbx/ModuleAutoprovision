d5c3ca214e97c92314300f1f804f4dfe86281659
Anonymous <noreply@weblate.org>
2023-12-15 12:07:40 +0000

    Translated using Weblate (Spanish)
    Currently translated at 96.5% (28 of 29 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/es/

--- Messages/es.php
+++ Messages/es.php
@@ -21,4 +21,23 @@ return [
 <br>Para configurar el teléfono, debe llamar a "<b>%extension%</b>" desde él, donde XXX es el número interno de la PBX.
 <br><br>
 La configuración automática sólo es posible para la red local de la empresa, para teléfonos <b>Yealink, Snom, Fanvil</b>.',
+    'mod_Autoprovision_phone_settings_title' => 'Ajustes de teléfono',
+    'mod_Autoprovision_phone_templates' => 'Plantillas de configuración',
+    'mod_Autoprovision_general_settings' => 'Configuración de URI',
+    'mod_Autoprovision_pnp' => 'Configuración PnP',
+    'mod_Autoprovision_addNew' => 'Agregar',
+    'mod_Autoprovision_templates_uri_uri' => 'URI',
+    'mod_Autoprovision_templates_uri_template' => 'Muestra',
+    'mod_Autoprovision_phone_settings_user' => 'Empleado',
+    'mod_Autoprovision_phone_settings_mac' => 'Dirección MAC',
+    'mod_Autoprovision_templates_header' => 'Al describir una plantilla, puede utilizar los siguientes parámetros: <b>{SIP_USER_NAME}</b> - nombre del empleado <b>{SIP_NUM}</b> - número interno (inicio de sesión) <b>{SIP_PASS}</b> - contraseña',
+    'mod_Autoprovision_templates_users_header' => 'Al describir una dirección MAC, se permite utilizar el símbolo <b>%</b>, que significa "cualquier conjunto de caracteres" <br>
+La plantilla <b>805e0c67%</b> coincidirá con <b>805e0c670001</b> y <b>805e0c670002</b>.',
+    'mod_Autoprovision_template_name' => 'Nombre',
+    'mod_Autoprovision_search_tags' => 'Buscar...',
+    'mod_Autoprovision_edit_template' => 'Editar una plantilla',
+    'mod_Autoprovision_end_edit_template' => 'Terminar de editar',
+    'mod_Autoprovision_other_pbx' => 'Directorio telefónico',
+    'mod_Autoprovision_other_pbx_name' => 'Nombre de la central telefónica',
+    'mod_Autoprovision_other_pbx_address' => 'Dirección de red PBX',
 ];
