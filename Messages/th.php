bb8a13c7ae96cfddd0f889defc63f8b2bae20765
Anonymous <noreply@weblate.org>
2024-02-23 02:56:51 +0000

    Translated using Weblate (Thai)
    Currently translated at 100.0% (29 of 29 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/th/

--- Messages/th.php
+++ Messages/th.php
@@ -1,1 +1,54 @@
 <?php
+return [
+    'mod_Autoprovision_other_pbx_name' => 'ชื่อของการแลกเปลี่ยนทางโทรศัพท์',
+    /*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+    'repModuleAutoprovision' => 'โมดูล - % ตัวแทน%',
+    'mo_ModuleAutoprovision' => 'โมดูลการกำหนดค่าโทรศัพท์อัตโนมัติ',
+    'BreadcrumbModuleAutoprovision' => 'โมดูลการกำหนดค่าโทรศัพท์อัตโนมัติ',
+    'SubHeaderModuleAutoprovision' => 'ช่วยในการตั้งค่าโทรศัพท์ SIP',
+    'mod_Autoprovision_Extension' => 'แม่แบบหมายเลขส่วนขยาย',
+    'mod_Autoprovision_pbx_host' => 'ที่อยู่เซิร์ฟเวอร์สำหรับการลงทะเบียนโทรศัพท์',
+    'mod_Autoprovision_mac_black' => 'บัญชีดำของที่อยู่ MAC ของโทรศัพท์',
+    'mod_Autoprovision_mac_white' => 'รายการที่อยู่ MAC ของโทรศัพท์สีขาว',
+    'mod_Autoprovision_additional_params' => 'ตัวเลือกพิเศษ',
+    'mod_Autoprovision_phone_settings_title' => 'การตั้งค่าโทรศัพท์',
+    'mod_Autoprovision_phone_templates' => 'แม่แบบการตั้งค่า',
+    'mod_Autoprovision_general_settings' => 'การตั้งค่า URI',
+    'mod_Autoprovision_pnp' => 'การตั้งค่า PnP',
+    'mod_Autoprovision_addNew' => 'เพิ่ม',
+    'mod_Autoprovision_templates_uri_uri' => 'ยูอาร์ไอ',
+    'mod_Autoprovision_templates_uri_template' => 'ตัวอย่าง',
+    'mod_Autoprovision_phone_settings_user' => 'พนักงาน',
+    'mod_Autoprovision_phone_settings_mac' => 'หมายเลขทางกายภาพ',
+    'mod_Autoprovision_template_name' => 'ชื่อ',
+    'mod_Autoprovision_search_tags' => 'ค้นหา...',
+    'mod_Autoprovision_edit_template' => 'การแก้ไขเทมเพลต',
+    'mod_Autoprovision_end_edit_template' => 'แก้ไขให้เสร็จสิ้น',
+    'mod_Autoprovision_other_pbx' => 'สมุดโทรศัพท์',
+    'mod_Autoprovision_other_pbx_address' => 'ที่อยู่เครือข่าย PBX',
+    'mod_Autoprovision_templates_header' => 'เมื่ออธิบายเทมเพลต คุณสามารถใช้พารามิเตอร์ต่อไปนี้: <b>{SIP_USER_NAME}</b> - ชื่อพนักงาน <b>{SIP_NUM}</b> - หมายเลขภายใน (เข้าสู่ระบบ) <b>{SIP_PASS}</b> - รหัสผ่าน',
+    'mod_Autoprovision_templates_users_header' => 'เมื่ออธิบายที่อยู่ MAC อนุญาตให้ใช้สัญลักษณ์ <b>%</b> - หมายถึง “ชุดอักขระใดก็ได้” <br>
+เทมเพลต <b>805e0c67%</b> จะตรงกับ <b>805e0c670001</b> และ <b>805e0c670002</b>',
+    'mod_Autoprovision_header' => 'หากเปิดใช้งานโมดูล บัญชี SIP "<b>apv-miko-pbx</b>" จะพร้อมใช้งานบน PBX
+<br>หากต้องการกำหนดค่าโทรศัพท์ของคุณโดยอัตโนมัติ คุณต้องรีเซ็ตเป็นการตั้งค่าจากโรงงาน
+<br>หากโทรศัพท์เชื่อมต่อกับ PBX เป็นครั้งแรก โทรศัพท์จะถูกลงทะเบียนในบัญชี "<b>apv-miko-pbx</b>"
+<br>ในการตั้งค่าโทรศัพท์ของคุณ คุณต้องโทรไปที่หมายเลข “<b>%extension%</b>” จากนั้น XXX คือหมายเลขภายในของ PBX
+<br><br>
+การกำหนดค่าอัตโนมัติสามารถทำได้เฉพาะกับเครือข่ายท้องถิ่นขององค์กร สำหรับโทรศัพท์ <b>Yealink, Snom, Fanvil</b>',
+];
