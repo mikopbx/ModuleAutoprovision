ad15165829d52f608b82ff7993383ff38d9840bc
Anonymous <noreply@weblate.org>
2024-07-30 07:23:59 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (29 of 29 strings)
    
    Translation: MIKOPBX/ModuleAutoprovision
    Translate-URL: https://weblate.mikopbx.com/projects/mikopbx/moduleautoprovision/hu/

--- Messages/hu.php
+++ Messages/hu.php
@@ -1,1 +1,54 @@
 <?php
+return [
+    'mod_Autoprovision_Extension' => 'Kiterjesztés száma sablon',
+    'mod_Autoprovision_other_pbx_name' => 'Alközpont neve',
+    'mod_Autoprovision_other_pbx_address' => 'PBX hálózati cím',
+    /*
+ * MikoPBX - free phone system for small business
+ * Copyright © 2017-2023 Alexey Portnov and Nikolay Beketov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+    'repModuleAutoprovision' => 'Modul – %repesent%',
+    'mo_ModuleAutoprovision' => 'Automatikus telefon konfigurációs modul',
+    'BreadcrumbModuleAutoprovision' => 'Automatikus telefon konfigurációs modul',
+    'SubHeaderModuleAutoprovision' => 'Segítség a SIP telefonok beállításában',
+    'mod_Autoprovision_pbx_host' => 'Telefonos regisztrációhoz szükséges szerver címe',
+    'mod_Autoprovision_mac_black' => 'A telefon MAC-címeinek feketelistája',
+    'mod_Autoprovision_mac_white' => 'A telefon MAC-címeinek fehér listája',
+    'mod_Autoprovision_additional_params' => 'Extra lehetőségek',
+    'mod_Autoprovision_phone_settings_title' => 'Telefon beállítások',
+    'mod_Autoprovision_phone_templates' => 'Beállítások sablonok',
+    'mod_Autoprovision_general_settings' => 'URI beállítások',
+    'mod_Autoprovision_pnp' => 'PnP beállítások',
+    'mod_Autoprovision_addNew' => 'Hozzáadás',
+    'mod_Autoprovision_templates_uri_uri' => 'URI',
+    'mod_Autoprovision_templates_uri_template' => 'Minta',
+    'mod_Autoprovision_phone_settings_user' => 'Munkavállaló',
+    'mod_Autoprovision_phone_settings_mac' => 'Mac cím',
+    'mod_Autoprovision_template_name' => 'Név',
+    'mod_Autoprovision_search_tags' => 'Keresés...',
+    'mod_Autoprovision_edit_template' => 'Sablon szerkesztése',
+    'mod_Autoprovision_end_edit_template' => 'Fejezze be a szerkesztést',
+    'mod_Autoprovision_other_pbx' => 'Telefonkönyv',
+    'mod_Autoprovision_templates_header' => 'A sablon leírásánál a következő paramétereket használhatja: <b>{SIP_USER_NAME}</b> - alkalmazott neve <b>{SIP_NUM}</b> - belső szám (bejelentkezés) <b>{SIP_PASS}</b> - jelszó',
+    'mod_Autoprovision_templates_users_header' => 'A MAC-cím leírásakor megengedett a <b>%</b> szimbólum használata – ez „bármely karakterkészlet” <br>
+A <b>805e0c67%</b> sablon egyezik a következővel: <b>805e0c670001</b> és <b>805e0c670002</b>',
+    'mod_Autoprovision_header' => 'Ha a modul engedélyezve van, az "<b>apv-miko-pbx</b>" SIP-fiók elérhetővé válik az alközponton
+<br>A telefon automatikus konfigurálásához vissza kell állítania a gyári beállításokat.
+<br>Ha a telefon először csatlakozik az alközponthoz, akkor az „<b>apv-miko-pbx</b>” fiókban lesz regisztrálva.
+<br>A telefon konfigurálásához fel kell hívnia a „<b>%extension%</b>” számot, ahol az XXX az alközpont belső száma.
+<br><br>
+Az automatikus konfigurálás csak a vállalati helyi hálózaton, a <b>Yealink, Snom, Fanvil</b> telefonokon lehetséges.',
+];
